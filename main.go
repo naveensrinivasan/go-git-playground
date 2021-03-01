bbde297985c7bb5a71b60f31604a086519ea35d8
naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-03-01 13:21:51 +0000

    Some tweaks

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"archive/tar"
-	"compress/gzip"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"path"
-	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/mholt/archiver"
@@ -77,14 +74,13 @@ func main() {
 		log.Panic(err)
 	}
 
-	// defer os.RemoveAll(dir)
-	// defer os.RemoveAll(tarDir)
+	defer os.RemoveAll(dir)
+	defer os.RemoveAll(tarDir)
 
 	// Clone the given repository to the given directory
 	r, err := git.PlainClone(dir, false, &git.CloneOptions{
-		URL:      fmt.Sprintf("http://%s/%s/%s", repo.Host, repo.Owner, repo.Repo),
-		Depth:    1, // Just fetch the last commit
-		Progress: os.Stdout,
+		URL:   fmt.Sprintf("http://%s/%s/%s", repo.Host, repo.Owner, repo.Repo),
+		Depth: 1, // Just fetch the last commit
 	})
 	if err != nil {
 		log.Panic(err)
@@ -107,14 +103,15 @@ func main() {
 		log.Panic(e)
 	}
 
-	lastcommit, err := commit.Author.When.MarshalBinary()
+	tpath := path.Join(tarDir, fmt.Sprintf("%s.tar.gz", repo.Repo))
+	// storing the last commit to the blob
+	err = bucket.Set(fmt.Sprintf("gitcache/%s/%s/lastcommit", repo.Owner, repo.Repo), []byte(fmt.Sprintf("%64b\n", commit.Author.When.Unix())))
 	if err != nil {
 		log.Panic(err)
 	}
 
-	tpath := path.Join(tarDir, fmt.Sprintf("%s.tar.gz", repo.Repo))
-	// storing the last commit to the blob
-	err = bucket.Set(fmt.Sprintf("gitcache/%s/%s/lastcommit", repo.Owner, repo.Repo), lastcommit)
+	now := time.Now().Unix()
+	err = bucket.Set(fmt.Sprintf("gitcache/%s/%s/lastsync", repo.Owner, repo.Repo), []byte(fmt.Sprintf("%64b\n", now)))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -144,112 +141,4 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-}
-
-func createArchive(files []string, buf io.Writer) error {
-	// Create new Writers for gzip and tar
-	// These writers are chained. Writing to the tar writer will
-	// write to the gzip writer which in turn will write to
-	// the "buf" writer
-	gw := gzip.NewWriter(buf)
-	defer gw.Close()
-	tw := tar.NewWriter(gw)
-	defer tw.Close()
-
-	// Iterate over files and add them to the tar archive
-	for _, file := range files {
-		err := addToArchive(tw, file)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func addToArchive(tw *tar.Writer, filename string) error {
-	// Open the file which will be written into the archive
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// Get FileInfo about our file providing file size, mode, etc.
-	info, err := file.Stat()
-	if err != nil {
-		return err
-	}
-
-	// Create a tar Header from the FileInfo data
-	header, err := tar.FileInfoHeader(info, info.Name())
-	if err != nil {
-		return err
-	}
-
-	// Use full path as name (FileInfoHeader only takes the basename)
-	// If we don't do this the directory strucuture would
-	// not be preserved
-	// https://golang.org/src/archive/tar/common.go?#L626
-	header.Name = filename
-
-	// Write file header to the tar archive
-	err = tw.WriteHeader(header)
-	if err != nil {
-		return err
-	}
-
-	// Copy file content to tar archive
-	_, err = io.Copy(tw, file)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func compress(src string, buf io.Writer) error {
-	// tar > gzip > buf
-	zr := gzip.NewWriter(buf)
-	tw := tar.NewWriter(zr)
-
-	// walk through every file in the folder
-	filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
-		// generate tar header
-		header, err := tar.FileInfoHeader(fi, file)
-		if err != nil {
-			return err
-		}
-
-		// must provide real name
-		// (see https://golang.org/src/archive/tar/common.go?#L626)
-		header.Name = filepath.ToSlash(file)
-
-		// write header
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-		// if not a dir, write file content
-		if !fi.IsDir() {
-			data, err := os.Open(file)
-			if err != nil {
-				return err
-			}
-			if _, err := io.Copy(tw, data); err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-
-	// produce tar
-	if err := tw.Close(); err != nil {
-		return err
-	}
-	// produce gzip
-	if err := zr.Close(); err != nil {
-		return err
-	}
-	//
-	return nil
 }
